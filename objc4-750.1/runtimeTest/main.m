a3e5c39146c09426dcd1ed5eb0fac6bbfd733337
wangmengnan <acbool@lonlife.cn>
2019-03-16 11:27:15 +0800

    init project

---
+++ objc4-750.1/runtimeTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  runtimeTest
+//
+//  Created by lonlife on 2019/3/15.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSObject *object = [[NSObject alloc] init];
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
